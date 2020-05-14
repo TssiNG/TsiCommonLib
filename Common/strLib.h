df1aeba76b7d9e837a5626e14992e4f9eeac96a6
August <714106674@qq.com>
2020-05-14 11:59:31 +0800

    添加了byte,word,dword转字符串的声明和byte,word转字符串的定义

--- Common/strLib.h
+++ Common/strLib.h
@@ -3,6 +3,14 @@
 
 #include <string>
 
+#ifdef WIN32
+#include <Windows.h>
+#else
+typedef unsigned char  BYTE;
+typedef unsigned short WORD;
+typedef unsigned long  DWORD;
+#endif
+
 namespace common
 {
 namespace str
@@ -10,6 +18,14 @@ namespace str
 void strReplace(std::string &src, const char *from, const char *to);
 
 double sci2db(const std::string &strSci);
+
+std::string byte2str(BYTE val);
+
+std::string word2str(WORD val);
+
+std::string dw2str(DWORD val);
+
+std::string bit_hex_to_str(size_t bit);
 }
 }
 
--- commonLib/commonLib/strLib.cpp
+++ commonLib/commonLib/strLib.cpp
@@ -49,3 +49,96 @@ double common::str::sci2db(const std::string &strSci)
 
   return dMntsa * dCoefficient;
 }
+
+std::string common::str::byte2str(BYTE val)
+{
+  std::string ret      = "";
+  size_t      high_bit = val / 16;
+  size_t      low_bit  = val % 16;
+
+  ret += bit_hex_to_str(high_bit);
+  ret += bit_hex_to_str(low_bit);
+
+  return ret;
+}
+
+std::string common::str::word2str(WORD val)
+{
+  std::string ret = "";
+
+  BYTE high_8_bit = 0xFF & (val >> 8);
+  BYTE low_8_bit  = 0xFF & val;
+
+  ret += bit_hex_to_str(high_8_bit / 16);
+  ret += bit_hex_to_str(high_8_bit % 16);
+  ret += bit_hex_to_str(low_8_bit / 16);
+  ret += bit_hex_to_str(low_8_bit % 16);
+
+  return ret;
+}
+
+std::string common::str::dw2str(DWORD val)
+{
+  return "";
+}
+
+std::string common::str::bit_hex_to_str(size_t bit)
+{
+  std::string ret;
+
+  switch (bit)
+  {
+    case 0:
+      ret = "0";
+      break;
+    case 1:
+      ret = "1";
+      break;
+    case 2:
+      ret = "2";
+      break;
+    case 3:
+      ret = "3";
+      break;
+    case 4:
+      ret = "4";
+      break;
+    case 5:
+      ret = "5";
+      break;
+    case 6:
+      ret = "6";
+      break;
+    case 7:
+      ret = "7";
+      break;
+    case 8:
+      ret = "8";
+      break;
+    case 9:
+      ret = "9";
+      break;
+    case 10:
+      ret = "A";
+      break;
+    case 11:
+      ret = "B";
+      break;
+    case 12:
+      ret = "C";
+      break;
+    case 13:
+      ret = "D";
+      break;
+    case 14:
+      ret = "E";
+      break;
+    case 15:
+      ret = "F";
+      break;
+    default:
+      ret = "";
+  }
+
+  return ret;
+}
